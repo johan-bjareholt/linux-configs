457de5aa78361f385c91c75e0f115f3de55b0b5f
Johan Bjäreholt <johan@johan-desktop.(none)>
2013-08-21 09:47:37 +0200

    Made a symlink script for my sub dropbox folders

---
+++ dropbox symlinks.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo Linking programming folder
+ln -s ~/Dropbox/Programmering/ ~/Programming
+echo Linking photos folder
+ln -s ~/Dropbox/Photos/ ~/Pictures/Dropbox
+echo Linking documents folder
+ln -s ~/Dropbox/Dokument/ ~/Documents
+
+
+echo Done!
