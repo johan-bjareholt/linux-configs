8f61bd68b79603691a9acc2a5331515fe27467e7
Johan Bjäreholt <johanbj@axis.com>
2018-11-09 13:48:33 +0100

    Simplified setup.sh
    Change-Id: I6ab5d600ef9d7c29fbc74a53f733de53483deb0b

--- setup.sh
+++ setup.sh
@@ -1,53 +1,20 @@
 #!/bin/bash
-cwd=$(pwd)
 
-dotfiles () {
-    ##########
-    # This function is based on a blogpost by Michael Smalley
-    # http://blog.smalleycreative.com/tutorials/using-git-and-github-to-manage-your-dotfiles/
-    #
-    # This function creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-    ##########
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
+pushd ./dotfiles
 
-    dir=$cwd/dotfiles
+# list of files/folders to symlink in homedir
+files="zshrc config/awesome config/awesome3.2 config/dwm Xdefaults vimrc xmonad xmonad-pantheon asoundrc ncmpcpp/config bspwm xlock"
 
-    # list of files/folders to symlink in homedir
-    files="zshrc config/awesome config/awesome3.2 config/dwm oh-my-zsh Xdefaults vimrc xmonad xmonad-pantheon asoundrc ncmpcpp/config velox.conf bspwm xlock mozilla/stylish"
-    
-    # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
-    cd $cwd/dotfiles
-    for file in $files; do
-        echo "Creating symlink to $file in home directory."
-        ln -s $dir/$file ~/.$file
-    done
-    if [ "$HOSTNAME" = "johan-desktop" ]
-      then
-        ln -s $dir/xbindkeysrc-desktop ~/.xbindkeysrc
-    elif [ "$HOSTNAME" = "johan-laptop" ]
-      then
-        ln -s $dir/xbindkeysrc-laptop ~/.xbindkeysrc
-    fi
-    cd $cwd
-}
-
-symlinks () {
-    echo "Creating you symlinks..."
-	echo "Linking notes folder"
-	ln -s ~/Dokument/Notes ~/Notes
-    echo "Linking bash scripts"
-    ln -s ~/Programming/Linux/linux-configs/scripts ~/Scripts
-    echo "Done with creating symlinks!"
-}
-
-echo Welcome to my linux config setup script!
-
-for section in dotfiles symlinks; do
-    echo "Would you like to setup $section? (y/n): "
-    read prompt
-    echo
-    if [ $prompt = y ]; then
-        `echo $section`
-    fi
+for file in $files; do
+    echo "Creating symlink to $file in home directory."
+    ln -s $(pwd)/$file ~/.$file
 done
 
-echo "Done!"
+if [ "$HOSTNAME" = "johan-desktop" ] ; then
+    ln -s xbindkeysrc-desktop ~/.xbindkeysrc
+elif [ "$HOSTNAME" = "johan-laptop" ] ; then
+    ln -s xbindkeysrc-laptop ~/.xbindkeysrc
+fi
+
+popd
